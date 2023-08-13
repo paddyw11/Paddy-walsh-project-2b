8013d2a5df44646dd4b27e367362b9679fd10518
paddyw11 <>
2023-08-13 23:50:22 +0000

    added js match feat and factored script

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,32 +4,47 @@ let hasFlippedCard = false;
 let firstCard, secondCard;
 
 function flipCard() {
-    this.classList.toggle('flip')
+    this.classList.add('flip')
 
     if (!hasFlippedCard) {
         // first click
-        hasFlippedCard - true;
-        firstCard -this;
-    }   else {
-        // second click
-        hasFlippedCard = false;
-        secondCard =this
+        hasFlippedCard = true;
+        firstCard = this;
 
-        if (firstCard.dataset.weather === 
-            secondCard.dataset.weather) {
-
-            firstCard.removeEventListener('click', flipCard);
-            secondCard.removeEventListener('click', flipCard);
-        } else {
-
-            setTimeout(() => {
-            firstCard.class.remove('flip');
-            secondCard.class.remove('flip');
-            }, 1500);
+        return;
         }
+
+        secondCard = this;
+        hasFlippedCard = false;
+        
+       
+        checkForMatch();
     }
-}   
 
+function checkForMatch() {
+    let isMatch = firstCard.dataset.weather === secondCard.dataset.weather;
 
+    isMatch ? disableCards() : unflipCards();
+    // do cards match?
+    if (firstCard.dataset.weather ===
+        secondCard.dataset.weather) {
+        // it's a match
+       disableCards();
+    } else {
+        unflipCards();
+    }
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500);
+}
 
 cards.forEach(card => card.addEventListener('click', flipCard))
