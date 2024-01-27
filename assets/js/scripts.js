499b9c7faadc7c9ff2165ac841c1e5b5260cc959
paddyw11 <>
2024-01-27 18:23:20 +0000

    fix of congrats modal and img png to webp

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,4 @@
-var cards = document.querySelectorAll('.card');
+var cards = document.querySelectorAll(".card");
 
 let hasFlippedCard = false;
 let lockBoard = false;
@@ -11,177 +11,165 @@ let closeCongratsBtn = document.getElementById("closeCongratsBtn");
 const revisionBtn = document.getElementById("revisionBtn"); // revision modal open
 const closeBtn = document.getElementById("closeBtn"); // revision modal close
 
-
 window.onload = initialiseBoard();
 
-
 // Revision modal
-revisionBtn.addEventListener('click', showRevisionModal); // listen for open click of revision modal
-closeBtn.addEventListener('click', closeRevisionModal); // listen for close revision modal button
+revisionBtn.addEventListener("click", showRevisionModal); // listen for open click of revision modal
+closeBtn.addEventListener("click", closeRevisionModal); // listen for close revision modal button
 
-closeCongratsBtn.addEventListener('click', closeCongratsModal); // listen for click on close button for congrats modal
-
+closeCongratsBtn.addEventListener("click", closeCongratsModal, resetBoard); // listen for click on close button for congrats modal
 
 function showRevisionModal() {
-    revisionModal.style.display = "block";
+  revisionModal.style.display = "block";
 }
 
 function closeRevisionModal() {
-    revisionModal.style.display = "none";
+  revisionModal.style.display = "none";
 }
 
 function initialiseBoard() {
-
-    cards.forEach((card) => card.addEventListener("click", flipCard));
-    shuffle();
+  cards.forEach((card) => card.addEventListener("click", flipCard));
+  shuffle();
 }
 
 function flipCard() {
-    if (lockBoard) return;
-    if (this === firstCard) return;
-    this.classList.add("flip");
-    playAudio(this);
+  if (lockBoard) return;
+  if (this === firstCard) return;
+  this.classList.add("flip");
+  playAudio(this);
 
-    if (!hasFlippedCard) {
-        // first click
-        hasFlippedCard = true;
-        firstCard = this;
-        moveCounter();
-        return;
-    }
-    hasFlippedCard = false;
-    secondCard = this;
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
     moveCounter();
-    checkForMatch();
-    checkWin();
+    return;
+  }
+  hasFlippedCard = false;
+  secondCard = this;
+  moveCounter();
+  checkForMatch();
+  checkWin();
 }
 
 function checkForMatch() {
-    let isMatch = firstCard.dataset.weather === secondCard.dataset.weather;
+  let isMatch = firstCard.dataset.weather === secondCard.dataset.weather;
 
-    isMatch ? disableCards() : unflipCards();
-    // do cards match?
-    if (isMatch) {
-        // it's a match
-        disableCards();
-    } else {
-        unflipCards();
-    }
+  isMatch ? disableCards() : unflipCards();
+  // do cards match?
+  if (isMatch) {
+    // it's a match
+    disableCards();
+  } else {
+    unflipCards();
+  }
 }
 
 function disableCards() {
-    firstCard.removeEventListener("click", flipCard);
-    secondCard.removeEventListener("click", flipCard);
-    
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
 }
 
 function unflipCards() {
-    lockBoard = true;
-    setTimeout(() => {
-        firstCard.classList.remove("flip");
-        secondCard.classList.remove("flip");
-        lockBoard = false;
-        //resetBoard();
-        
-    }, 1500);
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    lockBoard = false;
+    //resetBoard();
+  }, 1500);
 }
 
 //function to shuffle the locations of the cards
 function shuffle() {
-    cards.forEach(card => {
-        let randomPos = Math.floor(Math.random() * 16);
-        card.style.order = randomPos;
-    });
-};
-
-
-function moveCounter() {
-    if (hasFlippedCard) {
-        score++;
-        counter.innerHTML = score;
-
-        if (score === 1) {
-            second = 0;
-            minute = 0;
-            startTimer();
-        }
-    }
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 16);
+    card.style.order = randomPos;
+  });
 }
-// fucntion for the move counter
-//function moveCounter() {
- //   score++;
- //   counter.innerHTML = score;
 
-  //  if (score === 1) {
-  //      second = 0;
-  //      minute = 0;
- //       startTimer();
- //   }
-//}
+// funtion to add a move to the counter
+function moveCounter() {
+  if (hasFlippedCard) {
+    score++;
+    counter.innerHTML = score;
+
+    if (score === 1) {
+      second = 0;
+      minute = 0;
+      startTimer();
+    }
+  }
+}
 
 // timer
 var second = 0,
-    minute = 0;
+  minute = 0;
 var timer = document.querySelector(".timer");
 var interval;
 
-
 function startTimer() {
-    interval = setInterval(function () {
-        timer.innerHTML = "Elapsed Time: " + minute + " mins " + second + " secs ";
-        second++;
-        if (second == 60) {
-            minute++;
-            second = 0;
-        }
-        if (minute == 60) {
-            minute = 0;
-        }
-    }, 1000);
+  interval = setInterval(function () {
+    timer.innerHTML = minute + " : " + second;
+    second++;
+    if (second == 60) {
+      minute++;
+      second = 0;
+    }
+    if (minute == 60) {
+      minute = 0;
+    }
+  }, 1000);
 }
 
 // function to play audio on card flip
 function playAudio(card) {
-    const audio = card.querySelector("audio");
-    if (audio) {
-        audio.currentTime = 0;
-        audio.play();
-    }
+  const audio = card.querySelector("audio");
+  if (audio) {
+    audio.currentTime = 0;
+    audio.play();
+  }
 }
 
 // Congratulations modal
 function checkWin() {
-    const allFlipped = Array.from(cards).every(card => card.classList.contains("flip"));
-    
-    if (allFlipped) {
-        showCongratsModal();
-    }
+  const allFlipped = Array.from(cards).every((card) =>
+    card.classList.contains("flip")
+  );
+
+  if (allFlipped) {
+    finalTime = timer.innerHTML;
+    showCongratsModal();
+    document.getElementById("totalMoves").innerHTML = score;
+    document.getElementById("totalTime").innerHTML = finalTime;
+  }
 }
 
 function showCongratsModal() {
-    congratsModal.classList.add("show")
-    //congratsModal.style.visibility = "visible";
+  congratsModal.classList.add("show");
+  //congratsModal.style.visibility = "visible";
 }
- 
+
 function closeCongratsModal() {
-    congratsModal.classList.remove("show");
+  congratsModal.classList.remove("show");
 }
 
 // function to reset the board, timer and counter
 function resetBoard() {
-    lockBoard = false; 
-    hasFlippedCard = false;
-    [firstCard, secondCard] = [null, null];
-    score = 0;
-    counter.innerHTML = 0;
-    second = 0;
-    minute = 0;
-    clearInterval(interval);
-    timer.innerHTML = "Elapsed Time 0 mins 0 secs";
-    cards.forEach(card => card.classList.remove("flip"));
-    initialiseBoard();
+  lockBoard = false;
+  hasFlippedCard = false;
+  [firstCard, secondCard] = [null, null];
+  score = 0;
+  counter.innerHTML = 0;
+  second = 0;
+  minute = 0;
+  clearInterval(interval);
+  timer.innerHTML = "0 : 0";
+  cards.forEach((card) => card.classList.remove("flip"));
+  initialiseBoard();
+  closeCongratsModal();
 }
 
-const resetButton = document.getElementById("resetButton");
+const resetButton = document.getElementById("resetButton", "resetButton2");
 resetButton.addEventListener("click", resetBoard);
-
+resetButton2.addEventListener("click", resetBoard);
